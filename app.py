69b0339b208ec2ab0f95ee9c9e7e237807d8e684
Amoyzzz <dharkarajveer@gmail.com>
2024-09-28 14:20:40 -0400

    Pushing Basic System
    Pushing Basic System

--- app.py
+++ app.py
@@ -1,13 +1,34 @@
-<html>
-    
-</html>from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    names = ["ishaan", "rajveer"]
+# class User():
+
+@app.route('/', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        confirm = request.form['confirm']
+
+        if not validate():
+            flash('WHAT THE SIGMA DO YOU THINK YOU ARE DOING FOOL', 'error')
+        else:
+
+
+    names = []
+
     return render_template("index.html", names=names)
 
+def validate():
+    if request.form['password'] != request.form['confirm']:
+        return False
+    
+    
 if __name__ == '__main__':
     app.run(debug=True)
+    
