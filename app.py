adb88f00ab282bc5877697709b762130c5990207
Amoyzzz <dharkarajveer@gmail.com>
2024-09-28 14:45:30 -0400

    Created Password Validation
    Worked on password validation
    
    Co-Authored-By: ishaan-cherukuri <111136287+ishaan-cherukuri@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 app = Flask(__name__)
 
@@ -10,25 +8,48 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def register():
+    works = True
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
         confirm = request.form['confirm']
 
-        if not validate():
-            flash('WHAT THE SIGMA DO YOU THINK YOU ARE DOING FOOL', 'error')
+        if not validate_password():
+            works = False
+            flash('WHAT THE SIGMA DO YOU THINK YOU ARE DOING WITH YOUR PASSWORD FOOL', 'error')
         else:
+            #stuff goes here
+            flash("THIS IS SO SIGMA", 'lets gooooo')
+    return render_template("index.html", works=works)
 
-
-    names = []
-
-    return render_template("index.html", names=names)
-
-def validate():
-    if request.form['password'] != request.form['confirm']:
+def validate_password():
+    password = request.form['password']
+    special_characters = [
+    "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/",
+    ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
+    numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    if password != request.form['confirm']:
+        return False
+    if len(password) < 8 or len(password) > 15:
         return False
     
+    is_true = False
+    for letter in password:
+        if letter in special_characters:
+            return True
+    if not is_true:
+        return False
+       
+    is_true = False
+    for letter in password:
+        if letter in numbers:
+            return True
+    if not is_true:
+        return False
     
+def validade_emial():
+    email = request.form['password']
+
 if __name__ == '__main__':
     app.run(debug=True)
     
